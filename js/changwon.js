28aaedeb912386398854d990b9000e4311c60126
Universe <whdals7979@gmail.com>
2021-01-24 23:40:20 +0900

    창원대 와글 크롤링 및 출력

--- js/changwon.js
+++ js/changwon.js
@@ -1,10 +1,31 @@
-fetch(url["changwon_computer"])
-.then(res => res.json())
-.then(res => {
-    showChangwonComputer(res);
-})
+changwonRequest(url["changwon_computer"]);
 
-function showChangwonComputer(data){
+const menu_event = document.getElementsByClassName("changwon-menu");
+for(let i = 0; i < menu_event.length; i++){
+    menu_event[i].addEventListener("click", (ele) => {
+        if(ele.path[0].innerText.indexOf("창원대 컴공") != -1)
+            changwonRequest(url["changwon_computer"]);
+        else
+            changwonRequest(url["changwon_waggle"]);
+    })
+}
+
+
+function changwonRequest(url){
+    $(".changwon_data > tbody > tr").remove();
+    const html = `<tr>
+                        <td><img src="/image/loading2.gif" width="100px"></td>
+                    </tr>`
+    $(".changwon_data > tbody").append(html);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+        showChangwonData(res);
+    })
+}
+
+function showChangwonData(data){
     $(".changwon_data > tbody > tr").remove();
 
     const notice_count_max = 3;
--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ const url = {
 	"boannews" : "http://news.lactea.kr/boannews",
 	"dailysecu" : "http://news.lactea.kr/dailysecu",
 	"naver_realtime" : "http://news.lactea.kr/naver_realtime",
-	"changwon_computer" : "http://news.lactea.kr/changwon_computer"
+	"changwon_computer" : "http://news.lactea.kr/changwon_computer",
+	"changwon_waggle" : "http://news.lactea.kr/changwon_waggle"
 }
 
 const news_data = {
